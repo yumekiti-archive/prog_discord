f676c486e24259cc5191ae2bb627e5c4f3e0f20a
yumekiti <yumekiti120440@gmail.com>
2022-09-30 22:03:05 +0900

    `add` report

--- src/report.py
+++ src/report.py
@@ -1,10 +1,20 @@
-import json
+import openpyxl
 
-def main():
-  with open('tmp.json', 'r') as f:
-    data = json.load(f)
+def add_to_activity_report():
+  book = openpyxl.load_workbook('report.xlsx')
+  sheet = book.active
 
-  print(data)
+  i = 8
+  flag = True
+
+  while flag:
+    value = sheet.cell(row=i, column=1).value
+    if value == None or value == '日付': flag = False
+    else: i += 1
+
+  sheet.cell(row=i, column=1).value = 'Total'
+
+  book.save('report.xlsx')
 
 if __name__ == '__main__':
-  main()
+  add_to_activity_report()
